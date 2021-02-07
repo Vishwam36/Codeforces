a9ddc8919d9f889e47e25b443024322f49eaea61
Vishwam Shriram Mundada <58394139+Vishwam36@users.noreply.github.com>
2021-02-07 19:10:18 +0530

    Update 545C - Woodcutters.cpp

--- DP/545C - Woodcutters.cpp
+++ DP/545C - Woodcutters.cpp
@@ -21,6 +21,9 @@ int main()
     {
         cin >> x[i] >> h[i];
     }
+    // stay array stores max trees that can be cut if ith tree is not cut
+    // left array stores max trees that can be cut if ith tree is cut down to left (provided it can be cut down in left)
+    // right array stores max trees that can be cut if ith tree is cut down to right (provided it can be cut down in right)
     ll stay[n], left[n], right[n];
     stay[0] = 0;
     left[0] = 1;
@@ -34,8 +37,10 @@ int main()
     }
     for(int i = 1; i < n; ++i)
     {
+        // if tree can be cut down to left
         if(x[i] - h[i] > x[i-1])
         {
+            // if previous tree can be cut down to right simultaneously
             if(x[i-1] + h[i-1] < x[i] - h[i])
             {
                 left[i] = 1 + max(max(stay[i-1], right[i-1]), left[i-1]);
@@ -45,15 +50,20 @@ int main()
                 left[i] = 1 + max(left[i-1], stay[i-1]);
             }
         }
+        // if tree cant be cut down to left
         else
         {
             left[i] = max(max(left[i-1], right[i-1]), stay[i-1]);
         }
+        
         stay[i] = max(max(stay[i-1], left[i-1]), right[i-1]);
+        
+        // if tree can be can be cut down to right
         if(x[i] + h[i] < x[i+1] || i == n-1)
         {
             right[i] = 1 + max(max(stay[i-1], right[i-1]), left[i-1]);
         }
+        // if tree cant be cut down to right
         else
         {
             right[i] = max(max(left[i-1], stay[i-1]), right[i-1]);
